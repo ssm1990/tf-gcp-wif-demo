1ea771920d96d6cead7d21c673ba1281456cdc0d
ssm1990 <55357868+ssm1990@users.noreply.github.com>
2025-03-20 16:50:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,27 @@
+terraform {
+  backend "remote" {
+         # The name of your Terraform Cloud organization.
+    organization = "ssm52"
+
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "learn-terraform-github-actions"
+    }
+  }
+}
+
+#     # An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
+
+
 resource "google_storage_bucket" "my-bucket" {
   name          = "bkt-demo-000"
   location      = "us-central1"
-  project = "tt-dev-001"
+  project = "ss-dev-01"
   force_destroy = true
   public_access_prevention = "enforced"
 }
@@ -9,7 +29,7 @@ resource "google_storage_bucket" "my-bucket" {
 resource "google_storage_bucket" "my-bucket2" {
   name          = "bkt-demo-002"
   location      = "us-central1"
-  project = "tt-dev-001"
+  project = "ss-dev-01"
   force_destroy = true
   public_access_prevention = "enforced"
 }
